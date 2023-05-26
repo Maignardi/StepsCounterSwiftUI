c16f4aad8dedd02803cb21b9d0fca37c4643de5c
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-25 22:51:41 -0400

    Initial Commit

---
+++ Steps-calories/Steps_caloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steps_caloriesApp.swift
+//  Steps-calories
+//
+//  Created by matheus maignardi on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Steps_caloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
