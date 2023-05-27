61a3623df150fad644b24245d52b6151a4ec77a0
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-05-26 22:16:25 -0400

    first commit

--- Steps-calories/ContentView.swift
+++ Steps-calories/ContentView.swift
@@ -1,21 +1,94 @@
-//
-//  ContentView.swift
-//  Steps-calories
-//
-//  Created by matheus maignardi on 25/05/23.
-//
-
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    @State private var stepCount: Int = 0
+    @State private var caloriesBurned: Double = 0
+    
+    private let pedometer = CMPedometer()
+    private let calendar = Calendar.current
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Contador de Passos")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 50)
+            
+            Spacer()
+            
+            Text("\(stepCount)")
+                .font(.system(size: 100, weight: .bold, design: .default))
+                .padding(.bottom, 20)
+            
+            Text("Passos")
+                .font(.title)
+            
+            Spacer()
+            
+            Text("Calorias Gastas")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text("\(caloriesBurned, specifier: "%.2f")")
+                .font(.system(size: 40, weight: .bold, design: .default))
+            
+            Spacer()
         }
         .padding()
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: [Color.blue, Color.purple]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+        )
+        .onAppear(perform: {
+            startCountingSteps()
+        })
+        .onReceive(NotificationCenter.default.publisher(for: .NSCalendarDayChanged)) { _ in
+            resetCounters()
+        }
+    }
+    
+    private func startCountingSteps() {
+        resetCounters()
+        
+        let startOfDay = calendar.startOfDay(for: Date())
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+        
+        if CMPedometer.isStepCountingAvailable() {
+            pedometer.queryPedometerData(from: startOfDay, to: endOfDay) { pedometerData, error in
+                if let data = pedometerData {
+                    DispatchQueue.main.async {
+                        self.stepCount = data.numberOfSteps.intValue
+                        if let distance = data.distance {
+                            // Assumindo uma pessoa de 84 kg
+                            self.caloriesBurned = distance.doubleValue * 0.06 * 84.0 / 70.0
+                        }
+                    }
+                }
+            }
+            
+            pedometer.startUpdates(from: startOfDay) { pedometerData, error in
+                if let data = pedometerData {
+                    DispatchQueue.main.async {
+                        self.stepCount = data.numberOfSteps.intValue
+                        if let distance = data.distance {
+                            // Assumindo uma pessoa de 84 kg
+                            self.caloriesBurned = distance.doubleValue * 0.06 * 84.0 / 70.0
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func resetCounters() {
+        stepCount = 0
+        caloriesBurned = 0
     }
 }
 
